0e0d47ea2d9c5ce4d7e2737ca4b3885255a63916
Peter Domingues <peter@eridanusltda.com.br>
2024-05-15 16:58:25 -0300

    commented pre constructions

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -34,8 +34,8 @@ export default function HomeComponent() {
       </section>
       <Featured />
       <Divider />
-      <New />
-      <Divider />
+      {/* <New /> */}
+      {/* <Divider /> */}
       <Sold />
       <Cta />
       <CuratorWidget />
