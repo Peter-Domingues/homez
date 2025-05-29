9300e583040906cac4f214ad2ad97fb9319b87f7
Peter <peter@eridanusltda.com.br>
2025-05-29 13:17:41 -0300

    updated the url

--- src/api/properties.js
+++ src/api/properties.js
@@ -4,7 +4,7 @@ const getToken = async () => {
   try {
     const clientId = process.env.REACT_APP_REALTY_CLIENT_ID;
     const clientSecret = process.env.REACT_APP_REALTY_CLIENT_SECRET;
-    const authenticationUrl = "https://api.realtyfeed.com/token";
+    const authenticationUrl = "https://api.realtyfeed.com/v1/auth/token";
     const payload = `grant_type=client_credentials&client_id=${clientId}`;
 
     const auth = {
